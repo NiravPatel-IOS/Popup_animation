cd65642f7a1de4f669050489ee8ec0cb4be04424
NiravPatel-IOS <patel.nirav42@yahoo.co.in>
2014-02-07 01:41:31 -0800

    commit

---
+++ PopUp/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  PopUp
+//
+//  Created by bhavesh donga on 2/7/14.
+//  Copyright (c) 2014 bhavesh donga. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
